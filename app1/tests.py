d7677663aa84e16c2d7846a27575190594b37f0d
bharath-krishna <bharath.chakravarthi@gmail.com>
2014-11-20 05:54:49 +0000

    with failing test

--- app1/tests.py
+++ app1/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+"""
+This file demonstrates writing tests using the unittest module. These will pass
+when you run "manage.py test".
 
-# Create your tests here.
+Replace this with more appropriate tests for your application.
+"""
+
+from django.test import TestCase
+#from mylist import models
+
+
+class SimpleTest(TestCase):
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+#    def test_models(self):
+#        data = {"name": "battas", "date":"2222-11-11"}
+#        models.Schedule.objects.create(**data)  
+#        scs = models.Schedule.objects.all()
+#        print scs
+        
